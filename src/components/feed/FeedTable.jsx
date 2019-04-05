20f87e4324c42cd32be13cf7cbed7ad3c11747b8
Dmytro Khyzhniak <dmitriyKhiznyak@gmail.com>
2019-04-05 12:59:20 +0300

    Basic feed table layout

--- src/components/feed/FeedTable.jsx
+++ src/components/feed/FeedTable.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
 import FeedRepository from "../../repositories/FeedRepository";
 import SidebarStore from "../../stores/SidebarStore";
 import classNames from "classnames";
-import { Typography, Hidden } from "@material-ui/core";
 import DefaultImage from "../../assets/Overlay.svg";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { ArrowDropDown, ArrowDropUp } from "@material-ui/icons";
+import FacebookGrayIcon from "../../assets/FacebookGray.svg";
+import InstagramGrayIcon from "../../assets/InstagramGray.svg";
+import LinkedinGrayIcon from "../../assets/LinkedinGray.svg";
+import TwitterGrayIcon from "../../assets/TwitterGray.svg";
+import {
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Paper,
+  Link,
+  IconButton,
+  Menu,
+  MenuItem,
+  ButtonBase
+} from "@material-ui/core";
 
 const styles = theme => ({
   root: {
@@ -18,7 +31,6 @@ const styles = theme => ({
     overflowX: "auto"
   },
   drawerOpen: {
-    // width: `calc(100% - ${theme.sideBarWidth}px)`,
     marginLeft: theme.sideBarWidth + theme.spacing.unit * 3,
     transition: theme.transitions.create(["width", "margin-left"], {
       easing: theme.transitions.easing.sharp,
@@ -26,7 +38,6 @@ const styles = theme => ({
     })
   },
   drawerClose: {
-    // width: `calc(100% - ${theme.spacing.unit * 7}px)`,
     marginLeft: theme.spacing.unit * 10,
     transition: theme.transitions.create(["width", "margin-left"], {
       easing: theme.transitions.easing.sharp,
@@ -49,6 +60,12 @@ const styles = theme => ({
     maxHeight: "3.5em",
     overflowY: "hidden",
     textOverflow: "ellipsis"
+  },
+  shareButton: {
+    padding: theme.spacing.unit
+  },
+  shareIcon: {
+    marginRight: theme.spacing.unit * 3
   }
 });
 
@@ -60,7 +77,9 @@ class FeedTable extends Component {
       isLoaded: false,
       errorMessage: "",
       isSidebarOpen: SidebarStore.getState().isOpen,
-      items: []
+      items: [],
+      anchorAction: null,
+      anchorShare: null
     };
 
     this.onFeedLoaded = this.onFeedLoaded.bind(this);
@@ -87,6 +106,22 @@ class FeedTable extends Component {
     });
   };
 
+  handleActionClick = event => {
+    this.setState({ anchorAction: event.currentTarget });
+  };
+
+  handleActionClose = () => {
+    this.setState({ anchorAction: null });
+  };
+
+  handleShareClick = event => {
+    this.setState({ anchorShare: event.currentTarget });
+  };
+
+  handleShareClose = () => {
+    this.setState({ anchorShare: null });
+  };
+
   componentDidMount() {
     SidebarStore.on("change", this.onSidebarToggle);
     const feedRepo = new FeedRepository();
@@ -102,8 +137,17 @@ class FeedTable extends Component {
 
   render() {
     const { classes } = this.props;
-    const { errorMessage, isLoaded, items, isSidebarOpen } = this.state;
-    console.log({ errorMessage });
+    const {
+      errorMessage,
+      isLoaded,
+      items,
+      isSidebarOpen,
+      anchorAction,
+      anchorShare
+    } = this.state;
+    const isActionOpen = anchorAction ? true : false;
+    const isShareOpen = anchorShare ? true : false;
+
     if (errorMessage) {
       return (
         <Paper
@@ -159,16 +203,119 @@ class FeedTable extends Component {
                   <TableCell>
                     <div className={classes.postCell}>{row.post}</div>
                   </TableCell>
-                  <TableCell>{row.link}</TableCell>
+                  <TableCell>
+                    <Link href={row.link} target="_blank">
+                      {row.link}
+                    </Link>
+                  </TableCell>
                   <TableCell>
                     <img
                       src={row.image ? row.image : DefaultImage}
-                      alt="image"
+                      alt="feed"
                     />
                   </TableCell>
-                  <TableCell>Preview</TableCell>
-                  <TableCell>Share</TableCell>
-                  <TableCell>Action</TableCell>
+                  <TableCell>
+                    <Link href={row.link} target="_blank">
+                      Preview
+                    </Link>
+                  </TableCell>
+                  <TableCell>
+                    <ButtonBase
+                      className={classes.shareButton}
+                      aria-owns={isShareOpen ? "share-menu" : undefined}
+                      aria-haspopup="true"
+                      onClick={this.handleShareClick}
+                    >
+                      Share
+                      {isShareOpen ? <ArrowDropUp /> : <ArrowDropDown />}
+                    </ButtonBase>
+                    <Menu
+                      id="share-menu"
+                      anchorEl={anchorShare}
+                      open={isShareOpen}
+                      onClose={this.handleShareClose}
+                      getContentAnchorEl={null}
+                      anchorOrigin={{
+                        vertical: "bottom",
+                        horizontal: "left"
+                      }}
+                      transformOrigin={{
+                        vertical: "top",
+                        horizontal: "center"
+                      }}
+                    >
+                      <MenuItem
+                        key="share-facebook"
+                        onClick={this.handleShareClose}
+                      >
+                        <img
+                          src={FacebookGrayIcon}
+                          alt="facebook icon"
+                          className={classes.shareIcon}
+                        />
+                        Facebook
+                      </MenuItem>
+                      <MenuItem
+                        key="share-twitter"
+                        onClick={this.handleShareClose}
+                      >
+                        <img
+                          src={TwitterGrayIcon}
+                          alt="twitter icon"
+                          className={classes.shareIcon}
+                        />
+                        Twitter
+                      </MenuItem>
+                      <MenuItem
+                        key="share-linkedin"
+                        onClick={this.handleShareClose}
+                      >
+                        <img
+                          src={LinkedinGrayIcon}
+                          alt="linkedin icon"
+                          className={classes.shareIcon}
+                        />
+                        LinkedIn
+                      </MenuItem>
+                      <MenuItem
+                        key="share-instagram"
+                        onClick={this.handleShareClose}
+                      >
+                        <img
+                          src={InstagramGrayIcon}
+                          alt="instagram icon"
+                          className={classes.shareIcon}
+                        />
+                        Instagram
+                      </MenuItem>
+                    </Menu>
+                  </TableCell>
+                  <TableCell>
+                    <IconButton
+                      aria-label="More"
+                      aria-owns={isActionOpen ? "action-menu" : undefined}
+                      aria-haspopup="true"
+                      onClick={this.handleActionClick}
+                    >
+                      <MoreVertIcon />
+                    </IconButton>
+                    <Menu
+                      id="action-menu"
+                      anchorEl={anchorAction}
+                      open={isActionOpen}
+                      onClose={this.handleActionClose}
+                    >
+                      <MenuItem key="action1" onClick={this.handleActionClose}>
+                        Action 1
+                      </MenuItem>
+                      <MenuItem key="action2" onClick={this.handleActionClose}>
+                        Action 2
+                      </MenuItem>
+                      <MenuItem key="action3" onClick={this.handleActionClose}>
+                        Action 3
+                      </MenuItem>
+                    </Menu>
+                  </TableCell>
                 </TableRow>
               ))}
             </TableBody>
