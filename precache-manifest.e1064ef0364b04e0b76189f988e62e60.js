4d6b22bfd138bbec43ff278edbc352a94ce09f5c
Dmytro Khyzhniak <dmitriyKhiznyak@gmail.com>
2019-04-10 19:25:21 +0300

    Updates

---
+++ precache-manifest.e1064ef0364b04e0b76189f988e62e60.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a34b292f395c7c090d1b04599fccb8b",
+    "url": "/static/media/LinkedinGray.6a34b292.svg"
+  },
+  {
+    "revision": "71a11dbdd7866eb8eb04",
+    "url": "/static/css/main.43a2184d.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fea2370de4d23d004471",
+    "url": "/static/js/2.bafc6e46.chunk.js"
+  },
+  {
+    "revision": "55c705068519c793ee2d18d0491e2c6c",
+    "url": "/static/media/AvatarPlaceholder.55c70506.svg"
+  },
+  {
+    "revision": "a631de28458c1e719affef7051df80b4",
+    "url": "/static/media/Overlay.a631de28.svg"
+  },
+  {
+    "revision": "bba17588286bd043a6aeea71aba6365b",
+    "url": "/static/media/FacebookGray.bba17588.svg"
+  },
+  {
+    "revision": "e588ac391bb71b7ba8d9cdc55d2cacc0",
+    "url": "/static/media/InstagramGray.e588ac39.svg"
+  },
+  {
+    "revision": "71a11dbdd7866eb8eb04",
+    "url": "/static/js/main.83d3d741.chunk.js"
+  },
+  {
+    "revision": "d9878762a0f60dbae91b3371ae8d09cd",
+    "url": "/static/media/TwitterGray.d9878762.svg"
+  },
+  {
+    "revision": "355122a2d56ed9be89bc63adf6bbd0f5",
+    "url": "/static/media/facebook.355122a2.svg"
+  },
+  {
+    "revision": "c5fdceafb0e4cd78d18f183b742c0e8b",
+    "url": "/static/media/twitter.c5fdceaf.svg"
+  },
+  {
+    "revision": "a40bd3124def3d3abdd7260438968676",
+    "url": "/static/media/linkedin.a40bd312.svg"
+  },
+  {
+    "revision": "83b52887e44a6ed67e5e6bbfe9229331",
+    "url": "/static/media/instagram.83b52887.svg"
+  },
+  {
+    "revision": "06a26a6f160d78d937c86a862ddefd5f",
+    "url": "/static/media/Google.06a26a6f.svg"
+  },
+  {
+    "revision": "fc4e42b5b931a9e591d37b52fc690498",
+    "url": "/static/media/Exchange.fc4e42b5.svg"
+  },
+  {
+    "revision": "a7ae153df348b65b01202427456b2bda",
+    "url": "/index.html"
+  }
+];
